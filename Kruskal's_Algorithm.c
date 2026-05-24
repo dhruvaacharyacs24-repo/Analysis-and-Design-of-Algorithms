db517822b8cd7138978607efbafa1abc1baa4c28
Dhruva Acharya N <dhruvaacharya.cs24@bmsce.ac.in>
2026-05-24 20:19:33 +0530

    Implement Kruskal's Algorithm for MST

---
+++ Kruskal's_Algorithm.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int parent[20];
+
+int find(int x)
+{
+    while(parent[x])
+        x=parent[x];
+
+    return x;
+}
+
+int main()
+{
+    int n,i,j,min,u,v,a,b;
+    int edges=0,cost=0;
+
+    printf("Enter number of vertices: ");
+    scanf("%d",&n);
+
+    int g[20][20];
+
+    printf("Enter cost matrix:\n");
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&g[i][j]);
+
+    printf("Edges in MST:\n");
+
+    while(edges<n-1)
+    {
+        min=999;
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(g[i][j]<min)
+                {
+                    min=g[i][j];
+                    a=u=i;
+                    b=v=j;
+                }
+            }
+        }
+
+        u=find(u);
+        v=find(v);
+
+        if(u!=v)
+        {
+            parent[v]=u;
+
+            printf("%d -> %d = %d\n",a,b,min);
+
+            cost+=min;
+            edges++;
+        }
+
+        g[a][b]=g[b][a]=999;
+    }
+
+    printf("Minimum Cost = %d",cost);
+}
